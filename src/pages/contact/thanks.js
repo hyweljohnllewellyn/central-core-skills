0c2ceb0ac7b73a2eea6c107361c9d44282dbf631
Hywel Llewellyn <hywel.llewellyn@gmail.com>
2020-05-10 17:05:53 +0100

    Update thanks.js

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -17,7 +17,8 @@ export default () => (
       </p>
       <h1>Thank you!</h1>
       <p>
-        We appreciate that you have taken time to send us a message.
+        We appreciate that you have taken time to send us a message.  
+        We will contact you to discuss what you need and arrange a booking for a workshop or individual consultation.
         <br />
         Head back to{' '}
         <Link to="/">Home</Link>
